94614d29561ac19314c0db6ac382fa71e5e46640
Rafa <rafa.lozano.rolon@gmail.com>
2020-01-28 11:56:10 -0600

    ascii code

---
+++ ASCII_code/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    char c1=65;
+    char c2=112;
+    char c3=108;
+    char c4=101;
+    char c5=58;
+    char c6=')';
+    printf("%c%c%c%c%c%c%c", c1,c2,c2,c3,c4,c5,c6);
+    return 0;
+}
